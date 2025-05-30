6e5df1aa47f94be722770bf9b423d4bd104d4379
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-30 07:08:10 +0100

    Create paps_sidebar.php
    <?php
    
    return [
        'paps' => [
            'name' => 'Альянс PAPS',
            'icon' => 'fa fa-bar-chart',
            'route' => 'alliance-paps.index',
            'permission' => 'seatpaps.view',
        ],
    ];

---
+++ src/Config/paps_sidebar.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paps' => [
+        'name' => 'Альянс PAPS',
+        'icon' => 'fa fa-bar-chart',
+        'route' => 'alliance-paps.index',
+        'permission' => 'seatpaps.view',
+    ],
+];
