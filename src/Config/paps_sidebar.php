1f80e6200960b1454b2f68fc35dcc1181924d558
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-30 15:28:26 +0100

    Update paps_sidebar.php
    <?php
    
    return [
        'name' => 'Альянс статистика',
        'icon' => 'fa fa-bar-chart', // можно выбрать любую FontAwesome иконку
        'children' => [
            [
                'name' => 'Папсы',
                'icon' => 'fa fa-trophy',
                'route' => 'paps.stats', // имя route из web.php
            ],
            [
                'name' => 'Явка на форуме',
                'icon' => 'fa fa-comments',
                'route' => 'paps.forum',
            ],
            [
                'name' => 'Явка в Jabber',
                'icon' => 'fa fa-commenting',
                'route' => 'paps.jabber',
            ],
            [
                'name' => 'Явка в Mumble',
                'icon' => 'fa fa-microphone',
                'route' => 'paps.mumble',
            ],
        ],
    ];

--- src/Config/paps_sidebar.php
+++ src/Config/paps_sidebar.php
@@ -1,10 +1,28 @@
 <?php
 
 return [
-    'paps' => [
-        'name' => 'Альянс PAPS',
-        'icon' => 'fa fa-bar-chart',
-        'route' => 'alliance-paps.index',
-        'permission' => 'seatpaps.view',
+    'name' => 'Альянс статистика',
+    'icon' => 'fa fa-bar-chart', // можно выбрать любую FontAwesome иконку
+    'children' => [
+        [
+            'name' => 'Папсы',
+            'icon' => 'fa fa-trophy',
+            'route' => 'paps.stats', // имя route из web.php
+        ],
+        [
+            'name' => 'Явка на форуме',
+            'icon' => 'fa fa-comments',
+            'route' => 'paps.forum',
+        ],
+        [
+            'name' => 'Явка в Jabber',
+            'icon' => 'fa fa-commenting',
+            'route' => 'paps.jabber',
+        ],
+        [
+            'name' => 'Явка в Mumble',
+            'icon' => 'fa fa-microphone',
+            'route' => 'paps.mumble',
+        ],
     ],
 ];
