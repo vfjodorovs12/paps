844b97c7cadba336dfc0093d344db8b744488d9b
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-30 15:28:55 +0100

    Update web.php
    <?php
    
    use Illuminate\Support\Facades\Route;
    use Vfjodorovs12\Paps\Http\Controllers\AllianceStatsController;
    
    Route::group(['middleware' => ['web', 'auth']], function () {
        Route::get('/paps', [AllianceStatsController::class, 'index'])->name('paps.stats');
        Route::get('/paps/forum', [AllianceStatsController::class, 'forum'])->name('paps.forum');
        Route::get('/paps/jabber', [AllianceStatsController::class, 'jabber'])->name('paps.jabber');
        Route::get('/paps/mumble', [AllianceStatsController::class, 'mumble'])->name('paps.mumble');
    });

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Vfjodorovs12\Paps\Http\Controllers\AllianceStatsController;
 
-Route::group([
-    'middleware' => ['web', 'auth'],
-    'prefix' => 'alliance-paps',
-    'as' => 'alliance-paps.',
-], function () {
-    Route::get('/', [AllianceStatsController::class, 'index'])->name('index');
+Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::get('/paps', [AllianceStatsController::class, 'index'])->name('paps.stats');
+    Route::get('/paps/forum', [AllianceStatsController::class, 'forum'])->name('paps.forum');
+    Route::get('/paps/jabber', [AllianceStatsController::class, 'jabber'])->name('paps.jabber');
+    Route::get('/paps/mumble', [AllianceStatsController::class, 'mumble'])->name('paps.mumble');
 });
